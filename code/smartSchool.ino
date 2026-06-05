2e67185de55f301738e1c6b7fe52e4b77c754bd1
Deep Space Pi <45848107+deepspacepi@users.noreply.github.com>
2026-06-05 15:02:57 +0300

    Update smartSchool.ino

--- code/smartSchool.ino
+++ code/smartSchool.ino
@@ -1,1 +1,19 @@
+#include <Wire.h>
 
+void setup() {
+  Serial.begin(115200);
+while(!Serial); delay(5000);
+  Wire.begin();
+
+  Serial.println("Scanning...");
+  for (byte addr = 1; addr < 127; addr++) {
+    Wire.beginTransmission(addr);
+    if (Wire.endTransmission() == 0) {
+      Serial.print("Found device at 0x");
+      Serial.println(addr, HEX);
+    }
+  }
+  Serial.print("End Scanning");
+}
+
+void loop() {}
